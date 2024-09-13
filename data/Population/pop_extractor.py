d18127a452ae4d82ac23548e95f4ba0a8383cbc4
Thomas Woodley <t.woodley@student.unsw.edu.au>
2024-09-13 15:52:13 +1000

    Found a more accurate population data source and have interpolated daily from it

--- data/Population/pop_extractor.py
+++ data/Population/pop_extractor.py
@@ -1,30 +1,30 @@
 import pandas as pd
 
 #import the pop data that was manually changed to NSW only
-pop = pd.read_csv('data/Population/pop_his.csv', header=0)
+pop_interim = pd.read_csv('data/Population/quarterly_pop.csv', header=0)
 
-#transpose the dataframe
-pop = pop.T
-
-#make the first row the column names
-pop.columns = pop.iloc[0]
-
-#drop the first two rows
-pop = pop.drop(pop.index[0])
-pop = pop.drop(pop.index[0])
-
-#delete all null rows
-pop = pop.dropna()
-
-#delete all rows before 2009-12-31
-pop = pop[(pop.index >= '2009-12-31')]
-
-#save the file as yearly_pop_genders.csv
-#pop.to_csv('data/Population/yearly_pop_genders.csv')
+#change the Date column to datetime
+pop_interim['Date'] = pd.to_datetime(pop_interim['Date'], format='%b-%Y')
 
 #drop the Male and Female columns
-pop = pop.drop(columns=['Male','Female'])
+pop_interim.drop(columns=['Male','Female'], inplace=True)
+
+#create a new dataframe with a date range the same as pop_interim but with daily frequency
+pop = pd.date_range(start='1981-06-01', end='2021-07-01', freq='D')
+
+#left join the pop_interim dataframe with the new dataframe
+pop = pd.DataFrame(pop, columns=['Date'])
+pop = pd.merge(pop, pop_interim, on='Date', how='left')
+
+#fill the missing values with the linear interpolation between non-missing values
+pop['Population'] = pop['Population'].interpolate()
+
+pop.dropna(inplace=True)
+
+#change the Date range to the same as the other dataframes
+pop = pop[pop['Date'] >= '2010-01-01']
+pop = pop[pop['Date'] <= '2021-03-19']
 
 #save the file as yearly_pop.csv
-pop.to_csv('data/Population/yearly_pop_nsw.csv')
+pop.to_csv('data/Population/daily_pop_nsw.csv', index=False)
 
